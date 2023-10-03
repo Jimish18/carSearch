04c49860635b1f81ff0b7aaec34769fb54d584e7
jimish1806 <jimish957@gmail.com>
2023-10-03 17:45:15 +0530

    created context API for global variable management , set proper Folder Structure  , finalize css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
-import Carcard from './components/Carcard';
-import Navbar from './components/Navbar';
 import carData from './data';
 import React, { useEffect, useState } from 'react';
 import ReactPaginate from 'react-paginate';
+import { useSearch } from './context/Search';
+import Navbar from './components/Navbar/Navbar';
+import Carcard from './components/CarCard/Carcard';
+
+
 
 
 function App() {
 
+  // globally state of search with Context API
+  const [search, setSearch] = useSearch();
+
+    // pagination
   const [currentItems, setCurrentItems] = useState([]);
   const [pageCount, setPageCount] = useState(0);
   const [itemOffset, setItemOffset] = useState(0);
@@ -24,6 +31,18 @@ function App() {
     const newOffset = (event.selected * itemsPerPage) % carData.length;    
     setItemOffset(newOffset);
   };
+
+  // search function
+  useEffect(() => {
+    const filteredData = carData.filter((car) => {
+      return car.name.toLowerCase().includes(search.toLowerCase());
+    });
+    setCurrentItems(filteredData.slice(0, itemsPerPage));
+    setPageCount(Math.ceil(filteredData.length / itemsPerPage));
+    setItemOffset(0);
+
+
+  }, [search , setSearch]);
 
   
 
@@ -43,20 +62,22 @@ function App() {
       })}
       </div>
 
-      <ReactPaginate
-        breakLabel="..."
-        nextLabel="→"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={2}
-        pageCount={pageCount}
-        previousLabel="←"
-        renderOnZeroPageCount={null}
-        containerClassName='pagination'
-        pageLinkClassName='page-num'
-        previousLinkClassName='page-num'
-        nextLinkClassName='page-num'
-        activeLinkClassName='active'
-      />
+      <footer>
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel="→"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={2}
+          pageCount={pageCount}
+          previousLabel="←"
+          renderOnZeroPageCount={null}
+          containerClassName='pagination'
+          pageLinkClassName='page-num'
+          previousLinkClassName='page-num'
+          nextLinkClassName='page-num'
+          activeLinkClassName='active'
+        />
+      </footer>
     </>
 
   );
